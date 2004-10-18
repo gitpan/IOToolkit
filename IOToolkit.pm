a48253231e5fab29bd3953c59feb0de381dcf061
Markus Linke <markus.linke@linke.de>
2004-10-18 14:31:10 +0000

    Import of LINMAR/IOToolkit-1.12.2 from CPAN.
    gitpan-cpan-distribution: IOToolkit
    gitpan-cpan-version:      1.12.2
    gitpan-cpan-path:         LINMAR/IOToolkit/IOToolkit-1.12.2.tar.gz
    gitpan-cpan-author:       LINMAR
    gitpan-cpan-maturity:     released

--- IOToolkit.pm
+++ IOToolkit.pm
@@ -9,87 +9,106 @@ use base qw(Exporter);
 
 require Exporter;
 
-our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
+our @ISA         = qw(Exporter);
+our %EXPORT_TAGS = (
+    'all' => [
+        qw(
+
+          )
+    ]
+);
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our @EXPORT=qw(&logme &gettimestamp);
-our $VERSION = '1.10';
+our @EXPORT  = qw(&logme &gettimestamp);
+our $VERSION = '1.12.2';
 
-sub logme{
+sub logme {
 
-   my $severity = $_[0];
-   my $message  = $_[1];   
-   $message     = "" unless defined($message);
-      
-   if ($severity eq "open") {open LOGFILE,">>$message" or die "*** Fatal Error: Logfile $message cannot be opened.";}
-   elsif ($severity eq "close") {close LOGFILE or die "*** Fatal Error: Logfile cannot be closed.";}
-   
-   elsif ($main::getopt_loglevel =~ m/$severity/) {
-      my $timestamp = &gettimestamp();
-      my $line = "$timestamp [$main::programname] <$severity> $message\n";
-      if (not $main::getopt_loglevel =~ m/-/) {print $line;}
-      print LOGFILE $line;
-   }
-   
-   if (($severity eq "E") || ($severity eq "F")) {$main::error_occured++;}
-   
-   if ($severity eq "F") {  
-   close LOGFILE;
-   die "\n*** Fatal Error: $_[1]\n\n";
-   }
-   return 1;
+    my $severity = $_[0];
+    my $message  = $_[1];
+
+    if ( $severity eq "open" ) {
+
+        if ( !defined($message) ) {   		# If no filename is provided, use defaults
+            my $program = $0;         		# Script Name with path
+            $program =~ m/\/(.+)\.pl/i;    	# Without leading path and extension
+            my $logfilename = $1 . ".log";
+            $message = $logfilename;
+        }
+
+        open LOGFILE, ">>$message"
+          or die "*** Fatal Error: Logfile $message cannot be opened.";
+    }
+    elsif ( $severity eq "close" ) {
+        close LOGFILE or die "*** Fatal Error: Logfile cannot be closed.";
+    }
+
+    elsif (( $main::getopt_loglevel =~ m/$severity/ )
+        || ( $main::getopt_loglevel eq "all" ) )
+    {
+        my $timestamp = &gettimestamp();
+        my $line = "$timestamp [$main::programname] <$severity> $message\n";
+        if ( not $main::getopt_loglevel =~ m/-/ ) { print $line; }
+        print LOGFILE $line;
+    }
+
+    if ( ( $severity eq "E" ) || ( $severity eq "F" ) ) {
+        $main::error_occured++;
+    }
+
+    if ( $severity eq "F" ) {
+        close LOGFILE;
+        die "\n*** Fatal Error: $_[1]\n\n";
+    }
+    return 1;
 }
 
 sub gettimestamp {
-   my $format = $_[0];
-   my ($sec,$min,$hour,$mday,$mon,$year) = localtime;
-   $year=$year+1900;
-   $mon=$mon+1;
-   if ($mon<10) {$mon = "0".$mon;}
-   if ($mday<10) {$mday = "0".$mday;}
-   if ($hour<10) {$hour = "0".$hour;}
-   if ($min<10) {$min = "0".$min;}
-   if ($sec<10) {$sec = "0".$sec;}
-   if (defined($format)) {
-      if ($format eq "filename") {
-         return "$year"."$mon"."$mday"."$hour"."$min"."$sec";
-      }
-   } else {
-      return "$year-$mon-$mday $hour:$min:$sec";
-   }
+    my $format = $_[0];
+    my ( $sec, $min, $hour, $mday, $mon, $year ) = localtime;
+    $year = $year + 1900;
+    $mon  = $mon + 1;
+    if ( $mon < 10 )  { $mon  = "0" . $mon; }
+    if ( $mday < 10 ) { $mday = "0" . $mday; }
+    if ( $hour < 10 ) { $hour = "0" . $hour; }
+    if ( $min < 10 )  { $min  = "0" . $min; }
+    if ( $sec < 10 )  { $sec  = "0" . $sec; }
+    if ( defined($format) ) {
+
+        if ( $format eq "filename" ) {
+            return "$year" . "$mon" . "$mday" . "$hour" . "$min" . "$sec";
+        }
+    }
+    else {
+        return "$year-$mon-$mday $hour:$min:$sec";
+    }
 }
 
 sub trim {
     my @out = @_;
     for (@out) {
-        s/^\s+//;          # trim left
-        s/\s+$//;          # trim right
+        s/^\s+//;    # trim left
+        s/\s+$//;    # trim right
     }
-    return @out == 1 
-              ? $out[0]   # only one to return
-              : @out;     # or many
+    return @out == 1
+      ? $out[0]      # only one to return
+      : @out;        # or many
 }
 
 sub moduleinfo {
-   print "Directories searched:\n\t",
-   join ("\n\t" => @INC),
-   "\nModules loaded:\n\t",
-   join ("\n\t" => sort values %INC),
-   "\n";
-   return 1;
+    print "Directories searched:\n\t", join( "\n\t" => @INC ),
+      "\nModules loaded:\n\t", join( "\n\t" => sort values %INC ), "\n";
+    return 1;
 }
 
 1;
-    
+
 __END__
 
 =head1 NAME
 
-IOToolkit - Perl extension to create logfiles
+IOToolkit
 
 =head1 SYNOPSIS
 
@@ -103,11 +122,15 @@ Sample Script:
     package main; 
     use vars qw($getopt_loglevel $program $programname);
 
-    $program="logging";
-    $programname = $program.".pl";
+    my $program = $0;			# Script Name with path
+    $program =~ m/\/(.+)/i; 		# Only the scriptname
+    $programname = $1; 	
+    $program =~ m/(.+)\.pl/i; 		# Without the extension
+    $program = $1; 
+
     my $logfilename = $program.".log";
-    my $version = "1.00";
-    my $description = "Logging-Sample";
+    my $VERSION = "1.00";
+    my $description = "Description goes here";
 
     GetOptions("loglevel=s" => \$getopt_loglevel);
 
@@ -117,8 +140,9 @@ Sample Script:
        die "You did not provide any parameters. The program ended here.\n\n";
     } 
 
+    #logme("open");			# uses a default filename scriptname.log in the same dir
     logme("open",$logfilename);
-    logme("D","$programname V$version started --------------------------------------------------");
+    logme("D","$programname V$VERSION started --------------------------------------------------");
     logme("M","This is a MESSAGE");
     logme("D","This is a DEBUG-MESSAGE");
     logme("E","This is an ERROR-MESSAGE");
@@ -126,8 +150,8 @@ Sample Script:
     logme("C","This is a CONFIGURATION-MESSAGE");
     logme("M","FATAL- and SYSTEM-MESSAGES (F/S) are always logged.");
     logme("M","If the loglevel parameter contains - no messages are displayed.");
-    #logme("F","This is a FATAL-MESSAGE");
-    logme("D","$programname V$version ended   --------------------------------------------------");
+    #logme("F","This is a FATAL-MESSAGE which lets the program die");
+    logme("D","$programname V$VERSION ended   --------------------------------------------------");
     logme("close");
 
 This displays and creates a logfile like this:
@@ -144,19 +168,24 @@ This displays and creates a logfile like this:
 
 =head1 ABSTRACT
 
-Provides a human-readable logfile and is ment to replace "print" in your programs.
+IOToolkit - Perl extension to create logfiles
 
 =head1 DESCRIPTION
 
+Provides a human-readable logfile and is ment to replace "print" and "die" in your programs.
+
 This module was written to provide an easy way to log messages. It
-checks for an option --loglevel=EMCDEQ- where each character stands 
+checks for an option --loglevel=EMCDQ- where each character stands 
 for a certain level. e.g.
 
-   E=Error
-   S=System
-   M=Message
-   D=Debug
-   -=Silent
+   E   = Error
+   S   = System
+   M   = Message
+   D   = Debug
+   -   = Silent
+   all = All messages
+
+You can use all characters you would like to use. These are just examples.
 
 the minus ("-") has a special meaning: supresses output to the screen and
 ONLY logs them to the file. Please see the sample script for more details.
@@ -189,3 +218,4 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
 
 =cut
+
